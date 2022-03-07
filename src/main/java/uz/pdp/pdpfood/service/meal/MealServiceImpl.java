7644d1449458bf0e28e98fb2d7326e21bff0fe00
dev5654 <abdukarimov.inha@gmail.com>
2022-03-07 23:00:45 +0500

    Meal crud is done

--- src/main/java/uz/pdp/pdpfood/service/meal/MealServiceImpl.java
+++ src/main/java/uz/pdp/pdpfood/service/meal/MealServiceImpl.java
@@ -1,43 +1,61 @@
 package uz.pdp.pdpfood.service.meal;
 
-import uz.pdp.pdpfood.dto.meal.MealCreateDto;
-import uz.pdp.pdpfood.dto.meal.MealDto;
-import uz.pdp.pdpfood.dto.meal.MealUpdateDto;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uz.pdp.pdpfood.dto.meal.MealCreateDto;
+import uz.pdp.pdpfood.dto.meal.MealDto;
+import uz.pdp.pdpfood.dto.meal.MealUpdateDto;
+import uz.pdp.pdpfood.entity.meal.Meal;
+import uz.pdp.pdpfood.mapper.meal.MealMapper;
+import uz.pdp.pdpfood.repository.MealRepository;
+import uz.pdp.pdpfood.service.AbstractService;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service("mealService")
 @Slf4j
-@RequiredArgsConstructor
 @Transactional
-public class MealServiceImpl implements MealService {
+public class MealServiceImpl extends AbstractService<MealRepository, MealMapper> implements MealService {
+
+    public MealServiceImpl(MealRepository repository, MealMapper mapper) {
+        super(repository, mapper);
+    }
+
     @Override
     public Long create(MealCreateDto createDto) {
-        return null;
+        log.info("saving Meal {}", createDto.getName());
+        Meal meal = mapper.fromCreateDto(createDto);
+        repository.save(meal);
+        return meal.getId();
     }
 
     @Override
     public MealDto update(Long id, MealUpdateDto updateDto) {
-        return null;
+        Meal meal = mapper.fromUpdateDto(updateDto);
+        repository.save(meal);
+        return mapper.toDto(meal);
     }
 
     @Override
     public Void delete(Long id) {
+        repository.deleteById(id);
         return null;
     }
 
     @Override
     public List<MealDto> getAll() {
-        return null;
+        log.info("fetching all meals");
+        List<Meal> meals = repository.findAll();
+        return mapper.toDto(meals);
+
     }
 
     @Override
     public MealDto get(Long id) {
-        return null;
+        Optional<Meal> meal = repository.findById(id);
+        return mapper.toDto(meal.get());
     }
 }
